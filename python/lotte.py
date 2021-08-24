40415316163ff2e8eaf0ec128d115ea9a23e4f26
FabuKi-Xr <63010120@kmitl.ac.th>
2021-08-24 17:42:07 +0700

    solve test case

--- python/lotte.py
+++ python/lotte.py
@@ -1,14 +1,19 @@
 myLotte = input()
+isGetReward = False
 if(myLotte.isnumeric() and len(myLotte) == 6):
     if(myLotte == "046750"):
         print("คุณถูกรางวัลที่ 1")
-    elif(myLotte[0:3] == "421" or myLotte[0:3] == "666" ):
+        isGetReward = True
+    if(myLotte[0:3] == "421" or myLotte[0:3] == "666" ):
         print("คุณถูกรางวัลเลขหน้า 3 ตัว")
-    elif(myLotte[3:6] == "160" or myLotte[3:6] == "355" ):
+        isGetReward = True
+    if(myLotte[3:6] == "160" or myLotte[3:6] == "355" ):
         print("คุณถูกรางวัลเลขท้าย 3 ตัว")
-    elif(myLotte[4:6] == "23"):
+        isGetReward = True
+    if(myLotte[4:6] == "23"):
         print("คุณถูกรางวัลเลขท้าย 2 ตัว")
-    else:
+        isGetReward = True
+    if(not isGetReward):
         print("น่าเสียดาย คุณไม่ถูกรางวัล")
 else:
     print("หมายเลขไม่ถูกต้อง")
