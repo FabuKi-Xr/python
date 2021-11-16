ada6d4bf537d816a1a88b661ee61b7e78f5d6e0e
Xr <69309718+FabuKi-Xr@users.noreply.github.com>
2021-11-16 17:28:02 +0700

    update Lab10

---
+++ lab/Lab10/test.py
@@ -0,0 +1,23 @@
+# inp = ['3 2',1]
+# size,maxcoll = [int(i) for i in inp[0].split()]
+# print(size,maxcoll)
+# print(0**0)
+# a = [2]
+# a[0] = (1,2)
+# print(a)
+# if None in a:
+#     print("have none")
+# else:
+#     print("dont have none")
+# def nearDoublePrime(val):
+#         for i in range(val*2,val+40):
+#             for j in range(2,i):
+#                 if i % j == 0:
+#                     break
+#                 if i-1 == j:
+#                     return i
+# print(nearDoublePrime(23))
+# a = [None,2,None,1,3]
+# a.extend([None]*6)
+# print(a)
+print(int((4/47)*100))
